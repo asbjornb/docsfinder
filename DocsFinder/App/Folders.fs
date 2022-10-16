7dcf2c95cba25b63775153117af626f87c0883b9
Asbjørn Brandt <abjoernbrandt@gmail.com>
2022-10-16 21:08:57 +0200

    Move helper functions to separate module

---
+++ DocsFinder/App/Folders.fs
@@ -0,0 +1,20 @@
+﻿module Folders
+
+open System.IO
+open System
+
+//List folders
+let getFolders = Directory.EnumerateDirectories
+
+//Check for readme
+let checkForReadme folder = 
+    let readme = Path.Combine(folder, "readme.md")
+    (folder, File.Exists readme)
+    
+//Check readme age
+let checkReadmeAge folder = 
+    let readme = Path.Combine(folder, "readme.md")
+    let lastWriteTime = File.GetLastWriteTime readme
+    let now = DateTime.Now
+    (folder, (now - lastWriteTime).TotalDays)
+
--- DocsFinder/App/Program.fs
+++ DocsFinder/App/Program.fs
@@ -1,20 +1,5 @@
-﻿open System.IO
-open System
-
-//List folders
-let getFolders = Directory.EnumerateDirectories
-
-//Check for readme
-let checkForReadme folder = 
-    let readme = Path.Combine(folder, "readme.md")
-    (folder, File.Exists readme)
-    
-//Check readme age
-let checkReadmeAge folder = 
-    let readme = Path.Combine(folder, "readme.md")
-    let lastWriteTime = File.GetLastWriteTime readme
-    let now = DateTime.Now
-    (folder, (now - lastWriteTime).TotalDays)
+﻿open System
+open Folders
 
 [<EntryPoint>]
 let main argv =
