dea4f9af6b7fab0b5b346803275abfab95e79bda
Asbjørn Brandt <abjoernbrandt@gmail.com>
2022-10-16 08:55:14 +0200

    Pass input through

--- DocsFinder/App/Program.fs
+++ DocsFinder/App/Program.fs
@@ -7,17 +7,17 @@ let getFolders = Directory.EnumerateDirectories "C:\\code"
 //Check for readme
 let checkForReadme folder = 
     let readme = Path.Combine(folder, "readme.md")
-    File.Exists readme
+    (folder, File.Exists readme)
     
 //Check readme age
 let checkReadmeAge folder = 
     let readme = Path.Combine(folder, "readme.md")
     let lastWriteTime = File.GetLastWriteTime readme
     let now = DateTime.Now
-    (now - lastWriteTime).TotalDays
+    (folder, (now - lastWriteTime).TotalDays)
 
 [<EntryPoint>]
 let main argv = 
-    //Get folders, then check for readme and print input and output
-    getFolders |> Seq.map (fun folder -> folder, checkForReadme folder) |> Seq.iter (printfn "%A")
+    //Get folders, then check for readme and print results
+    getFolders |> Seq.map checkForReadme |> Seq.iter (printfn "%A")
     0 // return 0 exit code for success
