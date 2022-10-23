f8e17d875876ef0297c2b299f9305472c20ec5ad
Asbjørn Brandt <abjoernbrandt@gmail.com>
2022-10-23 22:35:04 +0200

    Add initial minimal argument parsing

--- DocsFinder/App/Program.fs
+++ DocsFinder/App/Program.fs
@@ -1,13 +1,27 @@
-﻿open System
-open Folders
+﻿open Folders
+
+type SkipMissingOption = SkipMissing | IncludeMissing
+
+type CommandLineOptions = {
+    skipMissing: SkipMissingOption;
+    path: string
+    }
+
+let printUsage() =
+    printfn "Usage: DocsFinder.exe [--skipMissing] [path]"
+
+//Parse args. Match empty array to default options. Match "--skipMissing" to skipmissing option and match path. PrintUsage if no case fits:
+let parseArgs(args: string[]) =
+    let defaultOptions = { skipMissing = IncludeMissing; path = "." }
+    match args with
+    | [||] -> defaultOptions
+    | [| "--skipMissing"; path |] -> { skipMissing = SkipMissing; path = path }
+    | [| "--skipMissing" |] -> { defaultOptions with skipMissing = SkipMissing }
+    | [| path |] -> { defaultOptions with path = path }
+    | _ -> printUsage(); exit 1
 
 [<EntryPoint>]
-let main argv =
-    // Get optional path positional argument
-    let path = 
-        if argv.Length > 0 then argv.[0]
-        else Environment.CurrentDirectory
-    
-    //Get folders, then check for readme and print results
-    getFolders path |> Seq.map checkForReadme |> Seq.iter (printfn "%A")
+let main args =
+    let options = parseArgs args
+    getFolders options.path |> Seq.map checkForReadme |> Seq.iter (printfn "%A")
     0 // return 0 exit code for success
