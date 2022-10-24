3c48603eea1271da29ae3d32d2861860e42fe973
Asbjørn Brandt <abjoernbrandt@gmail.com>
2022-10-24 07:56:50 +0200

    Attempt recursive argument parsing

--- DocsFinder/App/Program.fs
+++ DocsFinder/App/Program.fs
@@ -11,18 +11,21 @@ let printUsage() =
     printfn "Usage: DocsFinder.exe [--skipMissing] [path]"
 
 //Parse args. Match empty array to default options. Match "--skipMissing" to skipmissing option and match path. PrintUsage if no case fits:
-let parseArgs(args: string[]) =
-    let defaultOptions = { skipMissing = IncludeMissing; path = "." }
+let parseArgs (args: string list) =
+  let rec parse (options: CommandLineOptions) (args: string list) =
     match args with
-    | [||] -> defaultOptions
-    | [| "--skipMissing"; path |] -> { skipMissing = SkipMissing; path = path }
-    | [| "--skipMissing" |] -> { defaultOptions with skipMissing = SkipMissing }
-    | [| path |] -> { defaultOptions with path = path }
-    | _ -> printUsage(); exit 1
+    | [] -> options
+    | [x] -> { options with path = x }
+    | x :: xs ->
+      match x with
+      | "--skipMissing" -> parse { options with skipMissing = SkipMissing } xs
+      | _ -> printUsage(); exit 1
+
+  parse { skipMissing = IncludeMissing; path = "." } args
 
 [<EntryPoint>]
 let main args =
-    let options = parseArgs args
+    let options = parseArgs (args |> Array.toList)
     let filter (_, exists) = options.skipMissing=IncludeMissing || exists
     //getFolders at path, check if they have readme with checkForReadme, print results.
     getFolders options.path |> Seq.map checkForReadme |> Seq.filter filter |> Seq.iter (printfn "%A")
